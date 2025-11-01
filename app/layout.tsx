a77f71b8078beacc781a1e3ead1916fc355167f7
scheliwilli <karotiimars@gmail.com>
2025-11-01 23:45:11 +0700

    profile emoji was added to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata = {
       { url: "/icon.svg", type: "image/svg+xml" },
     ],
 }
+
+
 }
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -17,6 +19,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="bg-zinc-900 text-zinc-100 font-century antialiased min-h-screen">
         <div className="min-h-screen flex flex-col">
           <header className="h-14 flex items-center px-6 bg-zinc-800/80 border-b border-zinc-700/50">
+            {
+              localStorage.getItem("access_token") ? (
+                <h1>👤</h1>
+              ) : ""
+            }
             <div className="text-2xl font-semibold tracking-wide">Bratgram</div>
           </header>
           <main className="flex-1">{children}</main>
